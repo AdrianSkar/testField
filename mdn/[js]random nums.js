2846ac313293377ba6819fa9a444c085a323058a
AdrianSkar <adrian@adrianskar.com>
2021-01-06 16:33:17 +0100

    update: [js]random nums

--- mdn/[js]random nums.js
+++ mdn/[js]random nums.js
@@ -2,7 +2,7 @@
 
 console.time('a');
 
-let randM = Math.floor((Math.random() * 10) + 1);
+let randM = Math.floor((Math.random() * 10) + 1); // hardcoded; see formula below
 console.log('randMath:', randM);//
 
 console.timeEnd('a');
@@ -15,10 +15,12 @@ let randD = Date.now() % 10;
 console.log('randDate:', randD);
 
 console.timeEnd('b');
-
-
-
 //______________________________________________________________________________
+
+//* formula: Math.random() * (max - min + 1) + min
+let min = 1, max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
 
 /*
 Refs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
